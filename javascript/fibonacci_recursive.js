c8e4d9be8cf35a5765defaaa77d310ce82d46317
RomanLatipov <romanlatipov@gmail.com>
2024-06-27 22:18:21 -0400

    complete fibonacci-recursive lab

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,8 @@
 function fibonacci(n) {
-  // type your code here
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibonacci(n-1) + fibonacci(n-2);
 }
 
 if (require.main === module) {
